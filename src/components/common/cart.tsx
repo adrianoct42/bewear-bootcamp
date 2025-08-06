2cf2914f8af591b32417ce40072b7f521be2f444
Adriano Chamon <adrianochamontavares@gmail.com>
2025-08-06 19:09:05 -0300

    chore: add cart tables

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,3 +1,18 @@
+import { ShoppingBasketIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+
 export const Cart = () => {
-  return <div>Carrinho pai</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon">
+          <ShoppingBasketIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent></SheetContent>
+    </Sheet>
+  );
 };
